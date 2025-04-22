d4e990eb4fca9e403584799509828d36e01f2454
Khushbu K. <khushukat@gmail.com>
2025-04-22 17:21:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  //let counter = 5;
+
+  let [counter, setCounter] = useState(0);
+
+  const increaseCounter = () => {
+    console.log(counter);
+    if (counter < 20) {
+      counter = counter + 1;
+      setCounter(counter);
+    }
+  };
+
+  const decreaseCounter = () => {
+    console.log(counter);
+    if (counter > 0) {
+      counter = counter - 1;
+      setCounter(counter);
+    }
+  };
+
+  return (
+    <>
+      <h1>React hooks example</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={increaseCounter}>Increase</button>
+      <button onClick={decreaseCounter}>Decrease</button>
+    </>
+  );
+}
+
+export default App;
